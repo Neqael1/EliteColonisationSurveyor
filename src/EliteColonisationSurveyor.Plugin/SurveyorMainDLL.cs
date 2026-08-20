f5097acbefbdc8c619c7c52a4c8b10e45cbe1725
Andrew Cameron <acameron4@proton.me>
2026-08-20 20:47:54 +1000

    Add search cancellation controls

--- src/EliteColonisationSurveyor.Plugin/SurveyorMainDLL.cs
+++ src/EliteColonisationSurveyor.Plugin/SurveyorMainDLL.cs
@@ -25,7 +25,7 @@ namespace EliteColonisationSurveyor.Plugin
                 "Optimised colonisation candidate survey routes",
                 PanelIcon);
             callbacks.WriteToLog?.Invoke("Colonisation Surveyor extension loaded");
-            return "0.11.1";
+            return "0.11.2";
         }
 
         public void EDDRefresh(string commander, EDDDLLIF.JournalEntry latest) => SurveyorPanel.PublishLocation(latest);
